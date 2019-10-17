3bf0bfb10fbc184708ee12a20710b37e61b06550
rajaniket <aniketraj.go@gmail.com>
2019-10-17 22:42:33 +0530

    3rd program

---
+++ largest_f_3_inline.cpp
@@ -0,0 +1,17 @@
+//Write a program using inline function that obtains the largest of three numbers.
+
+#include <cmath>
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+
+inline float largest(float a,float b, float c)
+{
+        return (a>b)?(a>c)? a:c:(b>c)? b:c;
+}
+int main()
+{
+    float a;
+    a=largest(12,4,10.6);
+    cout<<a;
+}
