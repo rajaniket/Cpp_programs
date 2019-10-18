4bcd55cd1d2c9d76310921c6d5a212edeea8e81b
rajaniket <aniketraj.go@gmail.com>
2019-10-18 23:44:10 +0530

    8th program

---
+++ in1.cpp
@@ -0,0 +1,34 @@
+// write a program to calculate volume ,and also count the the number of times constructer is used.
+
+#include<iostream>
+using namespace std;
+class box {
+static int count;
+double length,width,height;
+public:
+box(double l=2.0, double b=3.0, double c=4.0){
+count++;
+cout<<"count = "<<count<<endl;
+cout<<"constructor called"<<endl;
+//cout<<"count = "<<count++<<endl; 
+length=l;
+width=b;
+height=c;
+}
+void c(){
+cout<<"Total Number of times is constructer used: "<<count<<endl;
+}
+double volume(){
+return length*width*height;
+}
+};
+int box :: count;
+
+int main()
+{
+box box1(1,2,3);
+box box2(3,2,3);
+cout<< "volume = "<<box1.volume()<<endl;
+cout<<"volume = "<< box2.volume()<<endl;
+box2.c();
+}
