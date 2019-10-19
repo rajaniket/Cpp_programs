f6e45bb50f5cf838fb1d585bf7e4217c87e53994
Aniket Raj <aniketraj.go@gmail.com>
2019-10-19 23:41:29 +0530

    9th program

---
+++ fibonacci.cpp
@@ -0,0 +1,20 @@
+// Fibonacci series using loop
+#include <iostream>
+using namespace std;
+int main()
+{
+   int x1=0,x2=1,x3,x;
+   cout<<"Type number of terms:-";
+   cin>>x;
+   cout<<endl;
+   cout<<"Fibonacci series upto "<<x<<" term :- ";
+   for(int i=0;i<x;i++){
+   cout<<x1<<"\t";
+   x3=x1+x2;
+   x1=x2;
+   x2=x3;
+   }
+}
+
+
+
