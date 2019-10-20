855d3a9fd5b35b93364b0e4bc5e8037f28844364
Aniket Raj <aniketraj.go@gmail.com>
2019-10-20 23:46:56 +0530

    12th program,add data objects

---
+++ eg5_9.cpp
@@ -0,0 +1,38 @@
+//Using friend function to add data objects from two different classes.
+
+#include<iostream>
+using namespace std;
+class s2;
+class s1
+{
+int data;
+public:
+void getdata(int x){
+data=x;
+}
+private:// friend function can be either private and public it works same for both;
+friend int add(s1 a,s2 b);
+};
+class s2
+{
+int data;
+public:
+void getdata(int x){
+data=x;
+}
+private:
+friend int add(s1,s2);
+};
+int add(s1 a, s2 b){
+int y;
+y=a.data + b.data;
+cout<<"addition of two objects of different class( "<<a.data<<" + "<<b.data<<" ) => "<<y<<endl;
+}
+int main()
+{
+s1 a;
+s2 b;
+a.getdata(5);
+b.getdata(2);
+add(a,b);
+}
