a9ede82561901cc68b670c6d390a15f4dbe773a1
Aniket Raj <aniketraj.go@gmail.com>
2019-10-22 23:50:46 +0530

    16th, Palindrome

---
+++ palindrome.cpp
@@ -0,0 +1,25 @@
+//Check a number is palindrome or not
+#include<iostream>
+#include<math.h>
+using namespace std;
+void palindrome(int n){
+int c,y=0,b ;
+c=n;
+while(n!=0){
+b=n%10;
+y=(y*10)+b;
+n=n/10;
+}
+if(c==y)
+cout<<c<<" is a palindrome"<<endl;
+else
+cout<<c<<" is not a palindrome"<<endl;
+}
+int main()
+{
+    int a;
+cout<<"type number:"<<endl;
+cin>>a;
+palindrome(a);
+}
+
