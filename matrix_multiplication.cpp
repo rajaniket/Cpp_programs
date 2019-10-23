9268fba0a9842bce748226469519617c5f4ec5a6
Aniket Raj <aniketraj.go@gmail.com>
2019-10-23 23:02:35 +0530

    17th,Matrix multiplication

--- matrix_multiplication.cpp
+++ matrix_multiplication.cpp
@@ -1,48 +1,51 @@
+//multiplication of two matrix
 #include<iostream>
 using namespace std;
-int main()
-{
-int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;
-cout<<"enter the number of row=";
-cin>>r;
-cout<<"enter the number of column=";
-cin>>c;
-cout<<"enter the first matrix element=\n";
-for(i=0;i<r;i++)
-{
-for(j=0;j<c;j++)
-{
+class matrix {
+int x,y;
+int a[10][10];
+public:
+void getmatrix(){
+cout<<"\nEnter Row and coloumn :"<<endl;
+cin>>x>>y;
+cout<<"Enter matrix elements"<<endl;
+for(int i=0;i<x;i++){
+for(int j=0;j<y;j++){
 cin>>a[i][j];
+}}}
+
+void multiply(matrix c,matrix b){
+int q,w;
+if(c.y==b.x){
+x=c.x;
+y=b.y;
+for(int i=0;i<x;i++){
+for(int j=0;j<y;j++){
+a[i][j]=0;
+for(int k=0;k<c.y;k++)
+a[i][j]=a[i][j]+(c.a[i][k])*(b.a[k][j]);
+}}
 }
+else cout<<"\nMatrix can't be multiply"<<endl;
 }
-cout<<"enter the second matrix element=\n";
-for(i=0;i<r;i++)
-{
-for(j=0;j<c;j++)
-{
-cin>>b[i][j];
-}
-}
-cout<<"multiply of the matrix=\n";
-for(i=0;i<r;i++)
-{
-for(j=0;j<c;j++)
-{
-mul[i][j]=0;
-for(k=0;k<c;k++)
-{
-mul[i][j]+=a[i][k]*b[k][j];
-}
-}
-}
-//for printing result
-for(i=0;i<r;i++)
-{
-for(j=0;j<c;j++)
-{
-cout<<mul[i][j]<<" ";
-}
-cout<<"\n";
-}
-return 0;
+
+void display(){
+for(int i=0;i<x;i++){
+cout<<endl;
+for(int j=0;j<y;j++)
+cout<<a[i][j]<<"\t";
+}cout<<endl;
+}};
+
+int main(){
+matrix a,b,c;
+a.getmatrix();
+cout<<"\n1st matrix you entered: \n";
+a.display();
+b.getmatrix();
+cout<<"\n2nd matrix you entered: \n";
+b.display();
+c.multiply(a,b);
+cout<<"\nMultiplication result: \n";
+c.display();
 }
