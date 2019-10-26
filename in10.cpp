7aead8b2ee52a539d5d86836b5180a3a1fb2d1b6
Aniket Raj <aniketraj.go@gmail.com>
2019-10-26 23:54:58 +0530

    28,Program to Convert a Binary Number to Decimal.

---
+++ in10.cpp
@@ -0,0 +1,19 @@
+// Program to Convert a Given Binary Number to its Decimal Equivalent.
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num, rem, temp, dec = 0, b = 1;
+    cout << "Enter the binary number : ";
+    cin >> num;
+    temp = num;
+    while (num > 0)
+    {
+        rem = temp % 10;
+        dec = dec + rem * b;
+        b *= 2;
+        temp /= 10;
+    }
+    cout << "The decimal equivalent of " << num << " is " << dec;
+    return 0;
+}
