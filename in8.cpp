c2ead67db4c3a932f37e89ebcaa49ddbb19be92e
Aniket Raj <aniketraj.go@gmail.com>
2019-10-26 23:20:23 +0530

    26, divide an int by 4 without using ‘/’ operator.

--- in8.cpp
+++ in8.cpp
@@ -1,10 +1,11 @@
-#include<iostream> 
-  
-using namespace std; 
-int main() 
-{ 
-   int n = 4; 
-   n = n >> 2; 
-   cout << n; 
-   return 0; 
-}   i
+//divide an integer by 4 without using ‘/’ operator.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n = 10;
+   n = n >> 2; // 0010----> 0100
+   cout << n;
+   return 0;
+}
