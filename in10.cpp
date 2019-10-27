588d91be34b3b862493559904fe26c2df0a0afc6
Aniket Raj <aniketraj.go@gmail.com>
2019-10-27 22:39:40 +0530

    Update in10.cpp

--- in10.cpp
+++ in10.cpp
@@ -1,19 +1,27 @@
 // Program to Convert a Given Binary Number to its Decimal Equivalent.
 #include<iostream>
+#include<math.h>
+#include<stdlib.h>
 using namespace std;
+int bin_dec(int a){
+int l=0,k,c=0;
+while(a!=0){
+  k=a%10;
+  if((k!=1)&&(k!=0) ){
+ cout<<"you have entered wrong decimal number"<<endl;
+   exit(0);}
+  a=a/10;
+  l=l+k*pow(2,c);
+  c++;
+}
+return l;
+}
+
 int main ()
 {
-    int num, rem, temp, dec = 0, b = 1;
+    int num,dec;
     cout << "Enter the binary number : ";
     cin >> num;
-    temp = num;
-    while (num > 0)
-    {
-        rem = temp % 10;
-        dec = dec + rem * b;
-        b *= 2;
-        temp /= 10;
-    }
+    dec=bin_dec(num);
     cout << "The decimal equivalent of " << num << " is " << dec;
-    return 0;
 }
