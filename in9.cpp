276ebf1e05f34ee8e2fd36af8e62fdb678a5d638
Aniket Raj <aniketraj.go@gmail.com>
2019-10-27 22:38:43 +0530

    27,program to convert decimal number to binary

--- in9.cpp
+++ in9.cpp
@@ -2,13 +2,22 @@
 #include <iostream>
 using namespace std;
 int dec_bin(int a){
-int k,l=0;
+int k,l=0,c=0;
 while(a!=0){
 k=a%2;
 a=a/2;
 l=l*10+k;
+c++;
 }
-return l;
+l=l+a;
+a=0;
+for(int i=0;i<c;i++){
+k=l%10;
+l=l/10;
+a=a*10+k;
+}
+
+return a;
 }
 
 int main(){
