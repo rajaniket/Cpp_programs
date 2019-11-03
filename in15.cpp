9034342b83453024472e349aa0e89d128f3ae0a9
Aniket Raj <aniketraj.go@gmail.com>
2019-11-03 23:47:41 +0530

    35

---
+++ in15.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+float comp(float a,float b,float c){
+//(a>b) ? (a>c) ? return a : return b : (b>c) ? return b : return c; // this is wrong because return a is statement not an expression
+return (a>b) ? (a>c) ? a : b : (b>c) ? b : c;
+}
+int main(){
+
+float a,b,c;
+cout<<"Type three numbers :";
+cin>>a>>b>>c;
+cout<<"\nThe greatest among three is: "<<comp(a,b,c);
+}
