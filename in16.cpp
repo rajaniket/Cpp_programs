50b1b83e4125aeb2da27718d78c8c06c5f1ce7d8
Aniket Raj <aniketraj.go@gmail.com>
2019-12-20 19:44:44 +0530

    E.g. Dynamic allocation of mem (Dynamic 1-D Array)

---
+++ in16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int*p = new int[10];   // allocation of memory
+    cout<<p<<endl;
+    for (int i = 0; i < 10; ++i) {
+            p[i]= i+1;}
+    for (int i = 0; i < 10; ++i) {
+            cout << p[i] << "  ";}
+    cout << endl;
+   delete [] p;            // freeing up memory for reuse (delete does not mean that it's content is deleted,it's means now it can be reallocate
+     cout<<p<<endl;
+    for (int i = 0; i < 10; ++i) {
+            cout << p[i] << "  ";}
+}
