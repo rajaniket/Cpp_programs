57df8d42c10d062e540d5e76a52e83df06db8e8f
Aniket Raj <aniketraj.go@gmail.com>
2019-12-21 00:11:21 +0530

    Constructing two dimensional array,use constructor

--- eg6_6.cpp
+++ eg6_6.cpp
@@ -1,38 +1,43 @@
-// Constructing two dimensional array
-// Constructing two dimensional array
-#include<iostream>
+// Constructing two dimensional array,use constructor
+//constructing matrix
+#include <iostream>
 using namespace std;
-class matrix{
-int m,n;
-int **p; //pointer to a matrix p[][]
-matrix(int r,int c){
-m=r;
-n=c;
-p=new int[m];
-for(int i=0;i<m<i++)
-p[i]=new int[n];
+class matrx{
+  int m,n,**p;
+  matrx(){
+  m=0;n=0;}
+public:
+matrx(int r,int c){
+m=r; n=c;
+p=new int*[m];
+int i,j;
+for(i=0;i<m;i++){
+    p[i]=new int[n];
+}}
+void getmatrx(int k,int l,int value)
+{
+    p[k][l]=value;
+
 }
-void get_element(int r,int c,int value){
-p[r][c]=value;
-}
-int & put_element(int r,int c){
-return p[r][c];
+int & putelement(int h,int j){
+return p[h][j];
 }
 };
-int main(){
-int row=2,col=2;
-int value;
-matrix a(row,col);
-for(int i=0;i<row;i++)
-for(int j=0;j<col;i){
-cout<<"type value"<<endl;
-cin>>value;
-cout<<endl;
-a.get_element(i,j,value);
+int main (){
+    int i,j,x;
+matrx a(3,3);
+cout<<"Enter element:"<<endl;
+for(i=0;i<3;i++){
+    for(j=0;j<3;j++){
+        cin>>x;
+        a.getmatrx(i,j,x);
+    }
 }
 cout<<endl;
-cout<<a.put_element(2,1)<<endl;
+for(i=0;i<3;i++){
+    for(j=0;j<3;j++){
+        cout<<a.putelement(i,j);  }
+    cout<<endl;
 }
-
+ cout<<a.putelement();
 }
-
