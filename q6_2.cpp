dded6e45c999e87fe810daa12f78c8366e633007
Aniket Raj <aniketraj.go@gmail.com>
2019-12-25 01:30:55 +0530

    adding two strings, include constructors

--- q6_2.cpp
+++ q6_2.cpp
@@ -1,4 +1,5 @@
-//program to represent a vector , and some functions to perform operations
+//Define a class String that could work as a user-defined string type. Include constructors that will enable us to create an uninitialized string
+//also include a function that adds two strings to make third string;
 #include<iostream>
 #include<cstring>
 using namespace std;
@@ -10,24 +11,28 @@ public:
     str(string k){
     p=new string(k);
     }
-   string add(const str &j){
+   str add(const str &j){
    str temp;
-   temp.*p=*p;
+   *(temp.p)=*p; //temp.*p will not work, for accessing pointer member use *(temp.p)
    string k;
-   k=temp.->p;
-   k->append(j);
-   temp.p=&k;
-   return temp;}
+   k=*(temp.p);
+   k.append(*(j.p));
+   *(temp.p)=k;
+  // (*(temp.p)).append(*(j.p));  it will work directly ,instead of using 16,17,18,19 use this.
+   return temp;
+   }
    void display(){
    cout<<"string : "<<*p<<endl;
    }};
    int main(){
-   str s1,s2("Aniket "),s3("Raj");
+   str s1,s2("Aniket "),s3("Raaaaaj");
    s1=s2.add(s3);
    s2.display();
    s3.display();
    s1.display();
 
    }
+
+
 
 
