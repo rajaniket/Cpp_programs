43d2af13d28a4d70ac2aac2307087ff8bbc6cdde
Aniket Raj <aniketraj.go@gmail.com>
2019-12-24 22:53:38 +0530

    *Adding two strings (concatenating two strings)

--- manipulating strings/str4.cpp
+++ manipulating strings/str4.cpp
@@ -1,15 +1,15 @@
-
+// Adding two strings (concatenating two strings)
 #include <iostream>
 #include<string.h>
 #include<string>
 using namespace std;
 int main()
 {
-   string *p=new string;
+   string *p=new string;  //string is like a class and *p is like a pointer object
    *p="hello ";
    string *k=new string("Raj");
    int j;
-   j=p->length();
+   j=p->length(); // here pointer object calling its member function
    cout<<*p<<endl<<*k<<endl<<j;
    p->append(*k);
    cout<<endl<<*p;
