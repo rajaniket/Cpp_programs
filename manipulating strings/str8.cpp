8917cb82b7172952ec6af6c1856f49baaaa92f29
Aniket Raj <aniketraj.go@gmail.com>
2019-12-26 21:15:19 +0530

    check sub-string is present inside a string or not

--- manipulating strings/str8.cpp
+++ manipulating strings/str8.cpp
@@ -1,11 +1,13 @@
 //To check whether a substring is present inside a string or not
 //make case insensitive search 
+//for searching the occurance of substring in a string use 'str1.find(str)' it will return position of substring from 0 to N
+//transform function =>applies an operation to all elements 
 #include<iostream>
 #include<algorithm>
 using namespace std;
 
 int string_find(string s1,string s){
-transform(s.begin(), s.end(), s.begin(),::tolower);
+transform(s.begin(), s.end(), s.begin(),::tolower);      
 transform(s1.begin(), s1.end(), s1.begin(),::tolower);
 cout<<s1<<endl<<s<<endl;
 int i=s1.find(s);
