78844c886a2294c337bdcf206991257464adcd34
Aniket Raj <aniketraj.go@gmail.com>
2019-12-31 23:30:10 +0530

    Add files via upload

---
+++ operator overloading and type conversions/eg7_1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class abc{
+int x,y;
+public:
+    abc(int a,int b){
+    x=a; y=b;
+    }
+    void display(){
+    cout<<"X="<<x<<"    "<<"Y="<<y<<endl;
+    }
+    void operator-()
+    {
+        x=-x;
+        y=-y;
+
+    }
+
+};
+int main(){
+abc s1(20,30);
+cout<<"Before using unary operator "<<endl;
+s1.display();
+-s1;
+cout<<"After using unary operator "<<endl;
+s1.display();
+}
+
+
+
+
