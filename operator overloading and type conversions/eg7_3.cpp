b21430cdfd65240596ee74115bfa92d0504253f2
Aniket Raj <aniketraj.go@gmail.com>
2020-01-02 00:30:03 +0530

    * overloading of input & output operator >> & <<

--- operator overloading and type conversions/eg7_3.cpp
+++ operator overloading and type conversions/eg7_3.cpp
@@ -1,10 +1,10 @@
-//Binary operator overloading using friend function
-//Sum of two complex number using friend function
+//overloading operator using friends
+//scalar multiplication of a vector,also including overloading of input & output operator >> & <<.
 #include<iostream>
 using namespace std;
 const int size=3;
 class vector{
-    float v[size]
+    float v[size];
 public:
     vector(){
     for(int i=0;i<size;i++){
@@ -12,46 +12,54 @@ public:
     }
     vector(float *a){
          for(int i=0;i<size;i++){
-        v[i]=a[i];}   
+        v[i]=a[i];}
         }
-    friend vector operator*(float a,vector);
-    friend vector operator*(vector,float a);
-    friend istream & operator>>(float *a);
-    friend ostream & operator<<(float *a);
+    friend vector operator*(float a,vector b);
+    friend vector operator*(vector b,float a);
+    friend istream & operator>>(istream &qin,vector &a);
+    friend ostream & operator<<(ostream &qout,vector &a);
 };
-    
+
     vector operator*(float a,vector b){
      vector temp;
      for(int i=0;i<size;i++){
             temp.v[i]=b.v[i]*a;}
      return temp;
      }
-    
+
     vector operator*(vector b, float a){
      vector temp;
      for(int i=0;i<size;i++){
             temp.v[i]=b.v[i]*a;}
      return temp;
      }
-     
-     istream &operator>>(istream &qin,vector &a){
+
+     istream & operator>>(istream &qin,vector &a){
      for(int i=0;i<size;i++){
-        qin>>a.v[i];
-        return qin;}
+        qin>>a.v[i];}
+        return qin;
      }
-     
-     ostream &operator>>(ostream &qout,vector &a){
+
+     ostream & operator<<(ostream &qout,vector &a){
       cout<<"(";
       for(int i=0;i<size;i++){
         if(i!=0)
         qout<<",";
         qout<<a.v[i];}
         qout<<")"<<endl;
-        return qin;}
+        return qout;}
      int main(){
-     
-     
-     
+     float m[size]={1.1,2,3};
+     vector c,d(m);
+     cout<<"enter all three elements of vector 'c' :"<<endl;
+     cin>>c;
+     cout<<"vector c= "<<c;
+     cout<<"vector d= "<<d;
+     vector a,b;
+     a=c*2;
+     b=3*d;
+     cout<<"vector a= "<<a;
+     cout<<"vector b= "<<b;
      }
 
 
