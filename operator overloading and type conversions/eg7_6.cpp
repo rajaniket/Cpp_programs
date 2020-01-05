9d63e144a61a6a98e94acf4534bba0b919526178
Aniket Raj <aniketraj.go@gmail.com>
2020-01-05 23:02:00 +0530

    Create eg7_6.cpp

---
+++ operator overloading and type conversions/eg7_6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class xyz{
+int num;
+public:
+    xyz(){
+    num=0;
+    }
+    xyz(int a)
+    {
+        num=a;
+    }
+    void show(){
+    cout<<"num :"<<num<<endl;
+    }
+    xyz* operator ->(){
+    return this;
+    }
+
+};
+int main(){
+xyz p;
+xyz *a=new xyz(5);
+p.show();
+a->show();
+p->show();
+}
+
+
+
