05558d88176a1a8e3002b75889ef48878a42f3fa
Aniket Raj <aniketraj.go@gmail.com>
2020-01-06 23:04:48 +0530

    Create eg7_7a.cpp

---
+++ operator overloading and type conversions/eg7_7a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class abc{
+int a;
+public:
+abc(){a=1;}
+abc(int k){a=k;}
+
+operator int(){
+return a*a;
+}
+
+};
+int main(){
+abc T(5),G;
+int k,u;
+k=T;
+u=int(G);
+cout<<"k="<<k<<endl;
+cout<<"u="<<u<<endl;
+cout<<"T="<<T<<endl;
+cout<<"G="<<G<<endl;
+}
