4af8c61d25a5a53b02a72a0db7828aa7ac593124
Aniket Raj <aniketraj.go@gmail.com>
2020-01-08 22:55:26 +0530

    Overload all the four arithmetic operators

---
+++ q7_1.cpp
@@ -0,0 +1,35 @@
+//Crate a class FLOAT that contains one float data member. Overload all the four arithmetic operators so that they operate on the objects of FLOAT.
+#include <iostream>
+using namespace std;
+class FLOAT{
+float num;
+public:
+FLOAT(){num=0;};
+FLOAT(float a){num=a;}
+float operator+(FLOAT k){
+return num+k.num;
+}
+float operator-(FLOAT k){
+return num-k.num;
+}
+float operator*(FLOAT k){
+return num*k.num;
+}
+float operator/(FLOAT k){
+return num/k.num;
+}
+operator float(){return num;}
+};
+int main(){
+FLOAT a(100),b(10),c;
+cout<<"a="<<a<<endl<<"b="<<b<<endl<<"c="<<c<<endl;
+c=a+b;
+cout<<"a+b="<<c<<endl;
+c=a-b;
+cout<<"a-b="<<c<<endl;
+c=a*b;
+cout<<"a*b="<<c<<endl;
+c=a/b;
+cout<<"a/b="<<c<<endl;
+}
+
