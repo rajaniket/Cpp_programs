5008a19d8e4fbe3949007e2b423fc94a602179b6
Aniket Raj <aniketraj.go@gmail.com>
2020-01-13 22:48:35 +0530

    q7_7

---
+++ operator overloading and type conversions/q7_7.cpp
@@ -0,0 +1,37 @@
+#include"iostream"
+#include"math.h"
+using namespace std;
+class cmp{
+float p;
+public:
+cmp(float h){p=h;}
+friend int operator==(cmp a,cmp b);
+friend int operator!=(cmp a,cmp b);
+friend int operator>=(cmp a,cmp b);
+friend int operator<=(cmp a,cmp b);
+};
+int operator==(cmp a,cmp b){
+if(a.p==b.p)return 1;
+else return 0;
+}
+int operator!=(cmp a,cmp b){
+if(a.p!=b.p)return 1;
+else return 0;
+}
+int operator>=(cmp a,cmp b){
+if(a.p>=b.p)return 1;
+else return 0;
+}
+int operator<=(cmp a,cmp b){
+if(a.p<=b.p)return 1;
+else return 0;
+}
+int main(){
+cmp x=1.414,y=1.414;
+if(x==y){cout<<"x=y"<<endl;}
+
+if(x!=y){cout<<"x!=y"<<endl;}
+if(x>=y){cout<<"x>=y"<<endl;}
+if(x<=y){cout<<"x<=y"<<endl;}
+
+}
