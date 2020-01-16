1b4eb43056f47c20cc6751463ccd18ec80fbd360
Aniket Raj <aniketraj.go@gmail.com>
2020-01-16 23:13:06 +0530

    Dynamic constructor example (constructor with new)

---
+++ Constructors and Destructors/eg6_5.cpp
@@ -0,0 +1,38 @@
+// Dynamic constructor example (constructor with new)
+#include<iostream>
+#include<cstring>
+using namespace std;
+class str{
+int len;
+char *name;
+public:
+    str(){
+        len=0;
+    name=new char[len+1];}
+str(char *s){   //constructor using new
+len=strlen(s);
+name=new char[len];
+strcpy(name,s);
+}
+void join(str &a,str &b){
+len=a.len+b.len;
+delete name;
+name=new char[len+1];
+strcpy(name,a.name);
+strcat(name,b.name);
+}
+void display(){
+cout<<"Name:"<<name<<endl;
+}
+};
+int main(){
+char *a="Aniket ";
+str n1(a),n2("Raj"),n3("hello "),n4,n5;
+n4.join(n1,n2);
+n5.join(n3,n2);
+n1.display();
+n2.display();
+n3.display();
+n4.display();
+n5.display();
+}
