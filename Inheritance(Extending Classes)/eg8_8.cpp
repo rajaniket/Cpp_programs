e2f5cb8bab686f4a07983a9889a71e827231d84c
Aniket Raj <aniketraj.go@gmail.com>
2020-01-19 22:56:13 +0530

    initialization list in constructors

---
+++ Inheritance(Extending Classes)/eg8_8.cpp
@@ -0,0 +1,51 @@
+//initialization list in constructors
+
+#include<iostream>
+using namespace std;
+class alpha{
+protected:
+    int x,q;    // x will initialized first then q will be
+public:
+    alpha(int c):x(c),q(2*x){    // alpha(int c):q(2),x(q) , will be wrong because x will be initialize first then q will initialize as per
+                                                                                                            // deceleration in data segment
+    cout<<"alpha initialized"<<endl;
+    }
+    void show(){
+        cout<<"X="<<x<<endl;
+        cout<<"Q="<<q<<endl;
+        }
+    };
+
+class beta{
+protected:
+    int y,w;
+public:
+    beta(int c):y(3*c),w(c+y){
+    cout<<"beta initialized"<<endl;
+    }
+    void show(){
+        cout<<"Y="<<y<<endl;
+        cout<<"W="<<w<<endl;}
+    };
+
+class gamma:public beta,public alpha{
+protected:
+    int z,e;
+public:
+    gamma(int a,int b,int c):alpha(a),beta(b),z(x*4)
+    {
+    e=c;
+    cout<<"gamma initialized"<<endl;
+    }
+    void show(){
+    alpha::show();
+    beta::show();
+    cout<<"Z="<<z<<endl;
+    cout<<"E="<<e<<endl;}
+    };
+
+int main(){
+gamma kk(1,2,3);
+kk.show();
+}
+
