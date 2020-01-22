d67c43aeeaea8c8045a4e7e64913e985db3df037
Aniket Raj <aniketraj.go@gmail.com>
2020-01-22 16:41:05 +0530

    bank account using inheritance

--- Inheritance(Extending Classes)/q8_1.cpp
+++ Inheritance(Extending Classes)/q8_1.cpp
@@ -1,6 +1,21 @@
-#include"iostream"
-#include"math.h"
-#include"string"
+/* Assume that a bank maintains two kinds of accounts for customers, one called as
+savings and the other as current account. The savings account provides compound interest
+and withdrawal facilities but no cheque book facility. The current account provides cheque
+book facility but no interest. Current account holders should also maintain a minimum
+balance and if the balance falls below this level a service charge is imposed.
+Create a class account that stores customer name, account number and type of account.
+From this derive the classes cur_acct and sav_acct to make them more specific to their
+requirements. Include necessary member functions in order to achieve the following tasks:
+(a) Accept the deposit from a customer and update the balance.
+(b) Display the balance.
+(c) Compute and deposit interest.
+(d) Permit withdrawal and update the balance.
+(e) Check for the minimum balance, impose penalty, necessary and update the balance.
+Do not use any constructors. Use member functions to initialize class members.
+*/
+
+#include<iostream>
+#include<string.h>
 #define charge 25  //
 #define rate 0.08 //8%
 #define min 3000
@@ -12,60 +27,62 @@ protected:
     void account_details();
     void show();
 };
-account::account_details(){
+  void account::account_details(){
     cout<<"Name :"<<endl;
     name=new string;
-    cin>>*name;
-    cout<<"Account Numbae :"<<endl;
+    cin.ignore();
+    getline(cin,*name);
+    cout<<"Account Number :"<<endl;
     acc_no=new string;
-    cin>>*acc_no;
+    getline(cin,*acc_no);
     cout<<"Type: "<<endl;
     type=new string;
-    cin>>*type;
+    getline(cin,*type);
     cout<<"Enter Today's date,month and year"<<endl;
     cin>>d>>m>>y;
     }
-account::show(){
+void account::show(){
     cout<<"Name: "<<*name<<endl;
     cout<<"Account Number: "<<*acc_no<<endl;
     cout<<"Account Type: "<<*type<<endl;
     cout<<"Date of creation "<<d<<"/"<<m<<"/"<<y<<endl;
     }
 
-class cur_acc:public account{
-float balance=0;
-public:
-    friend void create_account();
-    void show_balance();
-    friend void withdraw();
-    friend void deposit();
-};
 
 class sav_acc:public account{
 float balance=0;
-float interest_balance=0;
-int d1,m1,y1;
+int cal_date(int d2,int m2,int y2);
 public:
-    friend create_account();
+    void create_account();
     void show_balance();
-    friend void deposit();
-    void interest();
-   int cal_date(int d2,int m2,int y2);
+    void deposit();
+    void withdraw();
+    void interest(int d2,int m2,int y2);
+
+};
+
+class cur_acc:public account{
+float balance=0;
+public:
+     void create_account();
+     void show_balance();
+     void withdraw();
+     void deposit();
 };
 
 sav_acc::cal_date(int d2,int m2,int y2){
 int *month=new int[12]{31,28,31,30,31,30,31,31,30,31,30,31};   //defining no of days in each month
 
-if(d1==d2&&m1==m2&&y1==y2)   // date is same
+if(d==d2&&m==m2&&y==y2)   // date is same
 return 0;
 
-else if(m1==m2&&y1==y2)     // same month so only difference in days
-    return d2-d1;
+else if(m==m2&&y==y2)     // same month so only difference in days
+    return d2-d;
 
 else {                     // if month or year is not same
-int days=month[m1-1]-d1;   // Calculating number of days remaining for complete m1 month
-int h=m1+1;                // now from next month calculating days
-for(int i=y1;i<=y2;i++){
+int days=month[m-1]-d;   // Calculating number of days remaining for complete m1 month
+int h=m+1;                // now from next month calculating days
+for(int i=y;i<=y2;i++){
     for(int j=h;j<=12;j++){
         if(i==y2&&j==m2){    // in last month (month that is described in your 2nd date) only adding the days
         days+=d2;
@@ -77,23 +94,29 @@ for(int i=y1;i<=y2;i++){
     }
     h=1;                   // when year changes (j reset) it will start from month 1st=> January
     }
-    delete month[];
+    delete []month;
     return days;
 }
 }
 
 
-
-
-sav_acc::interest(int d2,int m2,int y2){
-int no_days;
-no_days=cal_date(d2,m2,y2);
-interest_balance=balance*rate*no_days/365;
+void sav_acc::interest(int d3,int m3,int y3){
+int *no_days;
+float *bal;
+no_days=new int;
+bal=new float;
+*no_days=cal_date(d3,m3,y3);
+cout<<"Number of days: "<<*no_days<<"\nInterest Rate: "<<rate<<endl;
+cout<<"Interest From Date "<<d<<"/"<<m<<"/"<<y<<" To date "<<d3<<"/"<<m3<<"/"<<y3<<endl;
+*bal=(balance*rate*(*no_days))/(float)365.0;
+cout<<"Interest balance= "<<*bal<<endl;
+delete no_days;
+delete bal;
 }
 
-void create_account(){
+void sav_acc::create_account(){
 float *p=new float;
-cout<<"Deposit an amount of 3000 rupee or more in order to open an account"<<endl;
+cout<<"Deposit an amount of "<<min<<" rupee or more in order to open an account"<<endl;
 cin>>*p;
 if(*p>=3000){
     balance+=*p;
@@ -101,43 +124,116 @@ if(*p>=3000){
     show();
     show_balance();
 }
-else cout<<"Deposit at least 3000 rupee"<<endl;
+else cout<<"Deposit at least "<<min<<" rupee"<<endl;
 }
 
-sav_acc::show_balance(){cout<<"Your Current Balance :"<<balance+interest_balance<<endl;}
+void cur_acc::create_account(){
+float *p=new float;
+cout<<"Deposit an amount of "<<min<<" rupee or more in order to open an account"<<endl;
+cin>>*p;
+if(*p>=3000){
+    balance+=*p;
+    account_details();
+    show();
+    show_balance();
+}
+else cout<<"Deposit at least "<<min<<" rupee"<<endl;
+}
 
-cur_acc::show_balance(){cout<<"Your Current Balance :"<<balance<<endl;}
+void sav_acc::show_balance(){cout<<"Your Current Balance :"<<balance<<endl;}
+void cur_acc::show_balance(){cout<<"Your Current Balance :"<<balance<<endl;}
 
-void withdraw(){
+void sav_acc:: withdraw(){
 float *p=new float;
 cout<<"Enter Amount :"<<endl;
 cin>>*p;
-if(*p<balance)
-    cout<<"Your Balance is Less than Withdrawal Amount,Transaction Unsuccessful"<<endl;
-else
-    if(balance==min){
+if(*p>balance){
+    cout<<"Your Balance is Less than Withdrawal Amount,Transaction Unsuccessful"<<endl;}
+else if(balance==min){
             char *w=new char;
     cout<<"Your balance is "<<balance<<" minimum balance charges of "<<charge<<" rupee will be imposed if balance is less than minimum balance of "<<min<<" rupee"<<endl;
     cout<<"Do you want to withdraw ? \nPress Y for yes and N for cancel"<<endl;
     cin>>*w;
-    if(*w==y||*w==Y){
+    if(*w=='y'||*w=='Y'){
     balance-=(*p);
-    cout<<"Type Today's date"<<endl;
-    cin>>d1>>m2>>y2;
     cout<<"Transaction Successful"<<endl;
     show_balance();}
     else cout<<"Transaction Canceled"<<endl;
     delete w;
 }
+else {
+    balance-=(*p);
+    cout<<"Transaction Successful"<<endl;}
  delete p;
 }
-void deposit(){
+
+void cur_acc:: withdraw(){
+float *p=new float;
+cout<<"Enter Amount :"<<endl;
+cin>>*p;
+if(*p>balance){
+    cout<<"Your Balance is Less than Withdrawal Amount,Transaction Unsuccessful"<<endl;}
+else if(balance==min){
+            char *w=new char;
+    cout<<"Your balance is "<<balance<<" minimum balance charges of "<<charge<<" rupee will be imposed if balance is less than minimum balance of "<<min<<" rupee"<<endl;
+    cout<<"Do you want to withdraw ? \nPress Y for yes and N for cancel"<<endl;
+    cin>>*w;
+    if(*w=='y'||*w=='Y'){
+    balance-=(*p);
+    cout<<"Transaction Successful"<<endl;
+    show_balance();}
+    else cout<<"Transaction Canceled"<<endl;
+    delete w;
+}
+else {
+    balance-=(*p);
+    cout<<"Transaction Successful"<<endl;}
+ delete p;
+}
+
+void sav_acc::deposit(){
 float *p=new float;
 cout<<"Enter Amount :"<<endl;
 cin>>*p;
 balance+=*p;
 show();
 delete p;
+}
+
+void cur_acc::deposit(){
+float *p=new float;
+cout<<"Enter Amount :"<<endl;
+cin>>*p;
+balance+=*p;
+show();
+delete p;
+}
+
+int main(){
+
+cur_acc ani;
+cout<<"Current Account"<<endl;
+ani.create_account();
+cout<<"\n\nDeposit \n\n"<<endl;
+ani.deposit();
+cout<<"\n\Withdraw \n\n"<<endl;
+ani.withdraw();
+cout<<"\n\show balance \n\n"<<endl;
+ani.show_balance();
+
+sav_acc raj;
+cout<<"\n\nSaving Account"<<endl;
+raj.create_account();
+cout<<"\n\nDeposit \n\n"<<endl;
+raj.deposit();
+raj.show_balance();
+cout<<"\n\Withdraw \n\n"<<endl;
+raj.withdraw();
+cout<<"\n\show balance \n\n"<<endl;
+raj.show_balance();
+cout<<"\n\interest \n\n"<<endl;
+raj.interest(1,1,2020);        // date should new as compared to account creation date
+
 }
 
 
