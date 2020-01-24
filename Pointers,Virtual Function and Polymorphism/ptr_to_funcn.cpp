faf0cd8ee965ed5847d5a852e03e3dadd533ec91
Aniket Raj <aniketraj.go@gmail.com>
2020-01-24 23:32:25 +0530

    switch case type functn usig Pointers to Functions

---
+++ Pointers,Virtual Function and Polymorphism/ptr_to_funcn.cpp
@@ -0,0 +1,23 @@
+// switch case type function using Pointers to Functions
+// array of pointers to function
+
+ #include<iostream>
+ using namespace std;
+ int add(int a,int b){
+     return a+b;
+ }
+ int sub(int a,int b){
+ return a-b;
+ }
+ int mul(int a,int b){
+ return a*b;
+ }
+ int main(){
+ int (*foo[])(int,int)={add,sub,mul};   // array of pointers to function
+ int q,a=10,b=7;
+ cout<<"Enter 1 for addition\nEnter 2 for subtraction\nEnter 3 for Multiplication"<<endl;
+ cin>>q;
+ if(q>0&&q<=3)
+    cout<<"Result: "<<(*foo[q-1])(a,b)<<endl;
+  else cout<<"Wrong key pressed"<<endl;
+ }
