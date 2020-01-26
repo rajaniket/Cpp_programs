27168e5bfa7e24ee32557be7601c3c80434647a1
Aniket Raj <aniketraj.go@gmail.com>
2020-01-26 23:30:56 +0530

    Example on pointers to function;

--- Pointers,Virtual Function and Polymorphism/eg9_7.cpp
+++ Pointers,Virtual Function and Polymorphism/eg9_7.cpp
@@ -1,7 +1,9 @@
+// Example on pointers to function;
 #include<iostream>
 #include<string.h>
 using namespace std;
-typedef void (*ptr)(int,int);
+typedef int (*ptr)(int,int);  // typedef is used to give new name to data type or functions
+                              // int (*ptr)(int,int); it is pointers to function 
  int add(int a,int b){
   return a+b;
  }
@@ -14,7 +16,7 @@ typedef void (*ptr)(int,int);
 int main(){
 ptr h;
 h=&add;
-cout<<*h(2,5)<<endl;
+cout<<h(2,5)<<endl;
 h=&sub;
 cout<<h(10,5)<<endl;
 h=&mul;
