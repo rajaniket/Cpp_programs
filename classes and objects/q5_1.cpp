dad10b25750317418e65c2cffe5e6ff0b647f73c
Aniket Raj <aniketraj.go@gmail.com>
2020-01-29 23:22:32 +0530

    Represent a bank account

---
+++ classes and objects/q5_1.cpp
@@ -0,0 +1,42 @@
+//Program to represent a bank account
+
+#include<iostream>
+using namespace std;
+class bank{
+string name,typ;
+int account_no; float balance;
+public:
+bank() {balance=0;}
+void getdata(){
+cout<<"\nAdd Name of Depositor: ";
+getline(cin,name);
+cout<<"Add type of account :";
+getline(cin,typ);
+}
+void deposit(float x){
+balance=x; }
+void withdraw(){
+cout<<"\nYour current balance is "<<balance<<" rupee. "<<endl;
+float drw;
+cout<<"Enter amount to withdraw : ";
+cin>>drw;
+balance=balance-drw;
+cout<<"Your current balance is "<<balance<<" rupee. "<<endl;
+}
+void display(){
+cout<<"\nThe Name of depositor is "<<name<<endl;
+cout<<"Account Type : "<<typ<<endl;
+cout<<"Your current balance is "<<balance<<" rupee. "<<endl;
+}
+};
+int main()
+{
+bank a;
+a.getdata();
+a.display();
+cout<<"After Deposit"<<endl;
+a.deposit(5000);
+a.display();
+a.withdraw();
+a.display();
+}
