12cb5b9a7890798615c0b5712e6f9f4dbe26feb3
Aniket Raj <aniketraj.go@gmail.com>
2020-02-01 23:48:14 +0530

    swap the private data of two objects of diff class

---
+++ Pointers,Virtual Function and Polymorphism/q9_4a.cpp
@@ -0,0 +1,48 @@
+// Using the concept of pointer write a function that swaps the private data values of two objects of different class type
+#include<iostream>
+using namespace std;
+class y;
+class x{
+    int s;
+    public:
+    x(int p):s(p){}
+    void display(){
+    cout<<"x="<<s<<endl;
+    }
+    friend void swap_obj(x *a,y *b);
+    friend void swap_obj(y *a,x *b);
+
+};
+class y{
+     int c;
+     public:
+    y(int p):c(p){}
+    void display(){
+    cout<<"y="<<c<<endl;
+    }
+    friend void swap_obj(x *a,y *b);
+    friend void swap_obj(y *a,x *b);
+};
+
+void swap_obj(x *a,y *b){
+int h;
+h=a->s;
+a->s=b->c;
+b->c=h;
+}
+void swap_obj(y *a,x *b){
+int h;
+h=a->c;
+a->c=b->s;
+b->s=h;
+}
+int main(){
+x a(5);
+y b(10);
+a.display();
+b.display();
+swap_obj(&a,&b);
+cout<<"\nafter swap\n"<<endl;
+a.display();
+b.display();
+}
