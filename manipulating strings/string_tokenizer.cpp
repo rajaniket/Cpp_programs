cca5de2a4e37059a37ad8c0f6fae1daee3eae23f
Aniket Raj <aniketraj.go@gmail.com>
2020-02-28 22:25:32 +0530

    Create string_tokenizer.cpp

---
+++ manipulating strings/string_tokenizer.cpp
@@ -0,0 +1,13 @@
+// String Tokenizer Function
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+int main(){
+char a[]={"Hi, My name is Aniket, My roll number is 31"};
+char *p;
+p=strtok(a," ,");
+
+p=strtok(NULL," ,");
+}
+}
