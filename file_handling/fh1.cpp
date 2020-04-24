ff2f7181d886efefe951e4202e74e8bc13473f1b
Aniket Raj <aniketraj.go@gmail.com>
2020-04-24 23:53:57 +0530

    opening and writing data in CSV (excel) file

--- file_handling/fh1.cpp
+++ file_handling/fh1.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main()
 {
 fstream new_file;
-new_file.open("hello.csv",ios::out);
-if(!new_file)
+new_file.open("hello.csv",ios::out); //creating new file
+if(!new_file) 
 {
 cout<<"File creation failed";
 }
 else
 {
 cout<<"New file created";
-new_file<<"123\n456\t789\n123";
+new_file<<"123\n456\t789\n123";// writing data in file
 new_file.close();
 }}
