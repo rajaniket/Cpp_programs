ca5ba9e00ae5fd8f9201c146e8b5cf1304098852
Aniket Raj <aniketraj.go@gmail.com>
2020-05-02 23:36:01 +0530

    camel case

--- array/arr12.cpp
+++ array/arr12.cpp
@@ -1,3 +1,23 @@
+/*
+One of the important aspect of object oriented programming is readability of the code. To enhance the readability of code, developers write function and variable names in Camel Case. You are given a string, S, written in Camel Case. FindAllTheWordsContainedInIt.
+Input Format
+A single line contains the string.
+Constraints
+|S|<=1000
+Output Format
+Print words present in the string, in the order in which it appears in the string.
+Sample Input
+IAmACompetitiveProgrammer
+Sample Output
+I
+Am
+A
+Competitive
+Programmer
+Explanation
+There are 5 words in the string.
+*/
+
 #include<iostream>
 #include<string.h>
 #include"algorithm"
