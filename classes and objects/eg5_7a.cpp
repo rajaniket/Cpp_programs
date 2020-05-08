0a748d8a32959ff1889c03055650bcfa91aed5da
Aniket Raj <aniketraj.go@gmail.com>
2020-05-08 23:55:48 +0530

    add two time to get new time

--- classes and objects/eg5_7a.cpp
+++ classes and objects/eg5_7a.cpp
@@ -34,3 +34,39 @@ b.gettime(3,64);
 c.addtime(a,b);
 c.showtime();
 }
+
+// method 2
+/* 
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Time {
+int hours,minutes;
+public:
+void gettime(int h,int m){
+hours=h;
+minutes=m;
+}
+void showtime()
+{
+
+cout<<"The time is "<<hours<<" hours and "<<minutes<<" minutes. "<<endl;
+}
+Time addtime(Time x)
+{
+Time Temp;
+Temp.hours=(x.minutes + minutes)/60;
+Temp.minutes=(x.minutes + minutes)%60;
+Temp.hours=Temp.hours + x.hours + hours;
+return Temp;
+}
+};
+int main() 
+{
+Time a,b,c;
+a.gettime(2,123);
+b.gettime(3,64);
+c=a.addtime(b);
+c.showtime();
+}
+*/
