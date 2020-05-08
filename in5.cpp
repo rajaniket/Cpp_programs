4f683b1b5781405b05f3611e1dbcea1a45593c97
Aniket Raj <aniketraj.go@gmail.com>
2020-05-08 23:52:25 +0530

    Swap the 2 variables without using extra variable

--- in5.cpp
+++ in5.cpp
@@ -12,3 +12,21 @@ b=a*b;
 a=b/a;
 cout<<"1st number is "<<a<<"\n2nd number is "<<b<<endl;
 }
+
+
+//To Swap the values of two variables without using any extra variable (method-2)
+/*
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"Enter 1st Number"<<endl;
+cin>>a;
+cout<<"Enter 2nd Number"<<endl;
+cin>>b;
+a=a+b;
+b=a-b;
+a=a-b;
+cout<<"1st number is "<<a<<"\n2nd number is "<<b<<endl;
+}
+*/
