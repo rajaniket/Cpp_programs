d7d96a344b3d81f91a0b108d3055af125ff80c24
Aniket Raj <aniketraj.go@gmail.com>
2020-05-16 23:58:57 +0530

    Update arr16.cpp

--- array/arr16.cpp
+++ array/arr16.cpp
@@ -17,3 +17,23 @@ for(int i=0;i<n;i++){
 
 }}
 }
+
+/*
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+int input[]={1,2,3,4};
+int n=4;
+for(int i=0;i<n;i++){
+    for(int j=0;j<n-i;j++){
+            cout<<endl;
+            int p=j;
+       for(int k=j;k>=0+j;k++){
+      cout<<input[p++]<<" ";
+}
+
+}}
+}
+*/
+
