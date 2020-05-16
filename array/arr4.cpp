d99b72a752e185bbca191fdf9f656d32111dae69
Aniket Raj <aniketraj.go@gmail.com>
2020-05-17 00:00:21 +0530

    Display Maximum subarray sum

--- array/arr4.cpp
+++ array/arr4.cpp
@@ -25,3 +25,22 @@ cout<<"maximum sum="<<max_sum<<endl;
 for(int i=left;i<=right;i++)
     cout<<arr[i]<<",";
 }
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+int arr[]={-2};
+int n=sizeof(arr)/sizeof(int);
+int current_sum=0,max_sum=INT_MIN,sum=0;
+for(int i=0;i<n;i++){
+        current_sum=current_sum+arr[i];
+        max_sum=max(current_sum,max_sum);
+        if(current_sum<0)
+            current_sum=0;
+    }
+
+cout<<"Maximum Sum: "<<max_sum;
+}
+*/
+
