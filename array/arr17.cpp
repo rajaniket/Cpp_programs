76f3604e67c98a85fde8fac0987bd3a6d7f2d057
Aniket Raj <aniketraj.go@gmail.com>
2020-05-18 23:52:17 +0530

    Create arr17.cpp

---
+++ array/arr17.cpp
@@ -0,0 +1,38 @@
+
+#include<iostream>
+#include<vector>
+#include<math.h>
+using namespace std;
+#define MAX 500
+int multiply(int n,int *a,int Count){
+int k,carry=0;
+for(int i=0;i<Count;i++){
+    k=(a[i]*n)+carry;
+    a[i]=k%10;
+    carry=k/10;
+}
+
+while(carry){
+a[Count]=carry%10;
+carry=carry/10;
+Count++;
+}
+return Count;
+}
+
+void fact(int n){
+int a[MAX];
+a[0]=1;
+int Count=1;
+for(int i=2;i<=n;i++){
+Count=multiply(i,a,Count);
+}
+for(int i=Count-1;i>=0;i--)
+cout<<a[i];
+}
+
+int main(){
+int n;
+cin>>n;
+fact(n);
+}
