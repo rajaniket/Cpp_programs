66a736ed5f2a69f8b749f70ff8c874ec74b0d428
Aniket Raj <aniketraj.go@gmail.com>
2020-07-07 23:59:57 +0530

    Macros function

--- Macro/macros example.cpp
+++ Macro/macros example.cpp
@@ -1,3 +1,8 @@
+/*
+
+Macros are a piece of code in a program which is given some name. Whenever this name is encountered by the compiler the compiler replaces the name with the actual piece of code.
+*/
+
 #include<stdio.h>
 #define M1(p) (p*p)
 #define SQUARE(n) ((n)*(n))
